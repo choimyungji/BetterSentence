bf39a4b48bdc000ccf7b2dcf0dc82fbdf7b972ba
Myungji Choi <choimyungji@gmail.com>
2021-02-05 23:09:26 +0900

    pick random sentence from array

--- BetterSentence/Sentence.swift
+++ BetterSentence/Sentence.swift
@@ -12,5 +12,6 @@ struct Sentence {
     var author: String
 }
 
-let exampleSentence: Sentence = Sentence(sentence: "GitHub앱 위젯이 생겼으니 매일매일 다시 해보자",
-                                         author: "Martin Choi")
+let exampleSentence: [Sentence] = [
+    Sentence(sentence: "GitHub앱 위젯이 생겼으니 매일매일 다시 해보자", author: "Martin Choi"),
+    Sentence(sentence: "있을때 잘하란 말을 구체적으로 실천할 수 있도록 계획해야겠다.", author: "Martin Choi")]
--- BetterSentence/ViewController.swift
+++ BetterSentence/ViewController.swift
@@ -10,17 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var sentenceLabel: UILabel!
-
     @IBOutlet weak var authorLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let sentence = exampleSentence
-        sentenceLabel.text = sentence.sentence
-        authorLabel.text = sentence.author
+        let sentence = exampleSentence.randomElement()
+        sentenceLabel.text = sentence?.sentence
+        authorLabel.text = sentence?.author
     }
-
-
 }
-
