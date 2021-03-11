4b592477b2707c3e90d042e2e61bb6a932755cbc
Myungji Choi <choimyungji@gmail.com>
2021-03-11 22:06:25 +0900

    setup collectionView

--- BetterSentence/ViewController.swift
+++ BetterSentence/ViewController.swift
@@ -20,5 +20,28 @@ class ViewController: UIViewController {
         let sentence = exampleSentence.randomElement()
         sentenceLabel.text = sentence?.sentence
         authorLabel.text = sentence?.author
+        
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        exampleSentence.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CSCollectionViewCell
+        
+        cell.lbl.text = exampleSentence[indexPath.row].sentence
+        
+        return cell
+    }
+    
+}
+
+class CSCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var lbl: UILabel!
+    
 }
